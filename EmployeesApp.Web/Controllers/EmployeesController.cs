6cbd5cc7d2566cae6cebd07ee3e60bdd7e2ca621
Hamredahl <JHamredahl@gmail.com>
2025-05-15 11:36:56 +0200

    Created Create view and added form.

--- EmployeesApp.Web/Controllers/EmployeesController.cs
+++ EmployeesApp.Web/Controllers/EmployeesController.cs
@@ -1,17 +1,30 @@
-﻿using EmployeesApp.Web.Services;
+﻿using EmployeesApp.Web.Models;
+using EmployeesApp.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 
-namespace EmployeesApp.Web.Controllers
-{
-    public class EmployeesController : Controller
-    {
-        EmployeeService employeeService = new EmployeeService();
+namespace EmployeesApp.Web.Controllers;
 
-        [Route("")]
-        public IActionResult Index()
-        {
-            var model = employeeService.GetAll();
-            return View(model);
-        }
+public class EmployeesController : Controller
+{
+    EmployeeService employeeService = new EmployeeService();
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        var model = employeeService.GetAll();
+        return View(model);
+    }
+
+    [HttpGet("create")]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost("create")]
+    public IActionResult Create(Employee employee)
+    {
+        employeeService.Add(employee);
+        return RedirectToAction("Index");
     }
 }
