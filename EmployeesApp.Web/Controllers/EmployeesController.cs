854d92818dfb61827f64ff44fbabc523c4c651b5
Hamredahl <JHamredahl@gmail.com>
2025-05-15 11:22:01 +0200

    Created EmployeesController and added Index method

---
+++ EmployeesApp.Web/Controllers/EmployeesController.cs
@@ -0,0 +1,17 @@
+﻿using EmployeesApp.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesApp.Web.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        EmployeeService employeeService = new EmployeeService();
+
+        [Route("")]
+        public IActionResult Index()
+        {
+            var model = employeeService.GetAll();
+            return View(model);
+        }
+    }
+}
