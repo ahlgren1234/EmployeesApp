0641c426980364d4e146ccadbac4d71f7f1558b0
Hamredahl <JHamredahl@gmail.com>
2025-05-15 11:18:02 +0200

    Created EmployeeServiec and added functionality.

---
+++ EmployeesApp.Web/Services/EmployeeService.cs
@@ -0,0 +1,21 @@
+﻿using EmployeesApp.Web.Models;
+
+namespace EmployeesApp.Web.Services
+{
+    public class EmployeeService
+    {
+        private List<Employee> employees = new List<Employee>();
+
+        public void Add(Employee employee)
+        {
+            employees.Add(employee);
+        }
+
+        public Employee[] GetAll() => employees
+            .OrderBy(e => e.Name)
+            .ToArray();
+
+        public Employee? GetById(int Id) => employees
+            .SingleOrDefault(e => e.Id == Id);
+    }
+}
